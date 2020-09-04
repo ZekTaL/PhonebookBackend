1189653b154fb1aeb38c0f92f1411de1d9f1bdcd
ZekTaL <francykiller@hotmail.it>
2020-09-04 19:38:40 +1000

    added build

---
+++ build/precache-manifest.f7e28d97e983a529968500eacf758f40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d934c52c0c976c730f440b89a87092f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c84fdea78b6f9614a720",
+    "url": "/static/css/main.ca6f2bf1.chunk.css"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84fdea78b6f9614a720",
+    "url": "/static/js/main.4f096821.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
